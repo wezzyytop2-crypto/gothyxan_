6d41cc6248e53f4bc333b1b16d7803dad27c20f6
mellory <wezzyytop2@icloud.com>
2025-12-10 13:25:54 +0200

    новый сайт

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7uFK25renleU9ZkTpDbY4zQo7hrCh-B0",
+    authDomain: "gothyxan.firebaseapp.com",
+    projectId: "gothyxan",
+    storageBucket: "gothyxan.firebasestorage.app",
+    messagingSenderId: "329511907152",
+    appId: "1:329511907152:web:2afaf437e6637581a30655",
+    measurementId: "G-FEH7NJX4MX"
+};
